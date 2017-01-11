27fe6f0bfc878aff2f87ae65057e2cfe76017c4f
tianyu <imty42@gmail.com>
2017-01-11 14:16:40 +0800

    rename x.plugin.zsh to xtools.plugin.zsh

--- xtools.plugin.zsh
+++ xtools.plugin.zsh
@@ -4,12 +4,11 @@ __TOOLBOX_DIR="${0:A:h}"
 
 function UTF8-GBK() {
     local get_new_name="$__TOOLBOX_DIR/get_new_name.py"
-    echo "${get_new_name}", $1
     _NEW_NAME=$(python ${get_new_name} -i $1 -f convert_results 2>/dev/null)
     if [ -z "${_NEW_NAME}" ]; then
         echo "Please check that if your file exits."
     else
-        echo "iconv -f UTF8 -t GB18030 $1 > ${_NEW_NAME}"
+        iconv -f UTF8 -t GB18030 $1 > ${_NEW_NAME}
         echo "the new file is:${_NEW_NAME}"
     fi
 }
